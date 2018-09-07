6c9678a750cdfaa180b2413a43fe53433d624233
sandhyaramavathu <42571611+sandhyaramavathu@users.noreply.github.com>
2018-09-07 19:43:28 +0530

    Create natural num.py

---
+++ natural num.py
@@ -0,0 +1,9 @@
+
+num = int(input("enter the number:"))
+if(num < 0):
+	print"the given number is negative"
+elif(num == 0):
+		print"the sum of numbers are zero"
+else:
+			sum = num*(num + 1)/2
+			print("the sum of numbers is",sum)
