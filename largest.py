3a91d8d4211dab7fdc9dab57e184988834619a59
sandhyaramavathu <42571611+sandhyaramavathu@users.noreply.github.com>
2018-09-07 12:36:52 +0530

    Create largest.py

---
+++ largest.py
@@ -0,0 +1,13 @@
+sravs1 = raw_input()
+sravs2 = raw_input()
+sravs3 = raw_input()
+if(sravs1 > sravs2)and(sravs1 > sravs3):
+	largest = sravs1
+	print"largest is:sravs1"
+elif(sravs2 > sravs1)and(sravs2 > sravs3):
+	largest = sravs2
+	print"largest is:sravs2"
+else:
+	largest = sravs3
+	print"largest is:sravs3"
+			
