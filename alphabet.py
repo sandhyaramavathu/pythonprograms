f26cfca0fdcbd9fd97ebf23298306f9964d5f170
sandhyaramavathu <42571611+sandhyaramavathu@users.noreply.github.com>
2018-09-07 12:16:58 +0530

    Create alphabet.py

---
+++ alphabet.py
@@ -0,0 +1,6 @@
+# your code goes here
+alp = raw_input()
+if alp in('a','b','c','d','e','f','g','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'):
+	print"alphabet"
+else:
+		print"not a alphabet"
